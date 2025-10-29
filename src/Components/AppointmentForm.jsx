b97a720eeceb7dd3862ea8f91829ebfbfb309228
Rupesh0497-gif <54547929+Rupesh0497-gif@users.noreply.github.com>
2025-10-29 19:55:06 -0400

    Initial Commit - Appointment Manager

---
+++ src/Components/AppointmentForm.jsx
@@ -0,0 +1,57 @@
+// AppointmentForm.js
+
+import React, { useState } from "react";
+
+const AppointmentForm = ({ addAppointment }) => {
+	const [name, setName] = useState("");
+	const [date, setDate] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		addAppointment({ name, date });
+		setName("");
+		setDate("");
+	};
+
+	return (
+		<div class="form-container">
+			<form onSubmit={handleSubmit}>
+				<div class="grid grid-cols-4">
+					<div class="col-span-4">
+						<label for="fname">Full Name</label>
+					</div>
+					<div class="col-span-8">
+						<input
+							type="text"
+							id="fname"
+							name="firstname"
+							placeholder="Your name.."
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+						/>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-25">
+						<label for="fname">Appointment Date: </label>
+					</div>
+					<div class="col-75">
+						<input
+							id="fname"
+							name="firstname"
+							placeholder="Your name.."
+							type="date"
+							value={date}
+							onChange={(e) => setDate(e.target.value)}
+						/>
+					</div>
+				</div>
+				<div class="row">
+					<input type="submit" value="Add Appointment" />
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default AppointmentForm;
